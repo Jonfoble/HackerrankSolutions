235508d2633c21992617c02f74761016ef21bcbb
Emre Guzel <Jonfoble@hotmail.com>
2022-10-26 08:14:32 +0300

    Hackerrank Solutions
    TheHurdleRace Solution...

---
+++ Solutions/TheHurdleRace/TheHurdleRace.cs
@@ -0,0 +1,59 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    public static int hurdleRace(int k, List<int> height)
+    {
+        int result = 0;
+        int max = 0;
+        for (int i = 0; i < height.Count; i++)
+        {
+            if (max < height[i])
+            {
+                max = height[i];
+            }
+        }
+        if (k < max)
+        {
+            result = max - k;
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> height = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(heightTemp => Convert.ToInt32(heightTemp)).ToList();
+
+        int result = Result.hurdleRace(k, height);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
