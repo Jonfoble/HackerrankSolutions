7677be77d7211df84cc378ac3604293a3a6d714b
Emre Guzel <Jonfoble@hotmail.com>
2022-10-26 08:34:30 +0300

    Hackerrank Solutions
    Hackerrank Solutions Uploaded...

---
+++ Solutions/PlusMinus/PlusMinus.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static void plusMinus(List<int> arr)
+    {
+        int k = arr.Count;
+        int positive = 0;
+        int negative = 0;
+        int zero = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            if (arr[i] > 0)
+            {
+                positive++;
+            }
+            else if (arr[i] < 0)
+            {
+                negative++;
+            }
+            else if (arr[i] == 0)
+            {
+                zero++;
+            }
+        }
+        Console.WriteLine((float)positive / k);
+        Console.WriteLine((float)negative / k);
+        Console.WriteLine((float)zero / k);
+    }
+}
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
